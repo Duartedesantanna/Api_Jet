8d9cb5013b62da4785f039e86175f3fdf7712fb5
Eduardo Duarte de Sant'Anna <duartedesantanna@gmail.com>
2022-04-07 15:32:32 -0300

    Implementação rota administrativa de edição de produto

--- src/controller/administrative/produtos.js
+++ src/controller/administrative/produtos.js
@@ -33,6 +33,36 @@ const cadastrarProduto = async (req, res) => {
 };
 
 const editarProduto = async (req, res) => {
+  const { id } = req.params;
+
+  const { nome, imagem, descricao, estoque, status, preco } = req.body;
+
+  const dadosEditadoProduto = {
+    nome,
+    imagem,
+    descricao,
+    estoque,
+    status,
+    preco
+  };
+
+  try {
+
+    await editarProdutoSchema.validate(req.body);
+
+    dadosEditadoProduto.preco = dadosEditadoProduto.preco.replace(",", ".");
+
+    const produtoEditado = await knex('produtos').where('id', id).update(dadosEditadoProduto);
+
+    if (!produtoEditado) {
+      return res.status(400).json({ mensagem: 'Não foi possivel editar o produto.' });
+    }
+
+    return res.status(200).json({ mensagem: 'Edição concluída com sucesso.' })
+
+  } catch (error) {
+    return res.status(400).json({ mensagem: error.message });
+  }
 };
 
 const excluirProduto = async (req, res) => {
