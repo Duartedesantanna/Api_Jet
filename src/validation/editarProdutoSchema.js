455bfcf87a5c82d4a53ab8339fcdfca6dea940aa
Eduardo Duarte de Sant'Anna <duartedesantanna@gmail.com>
2022-04-07 01:42:13 -0300

    Criação das validações (validation)

---
+++ src/validation/editarProdutoSchema.js
@@ -0,0 +1,24 @@
+const yup = require('./yup');
+
+const editarProdutoSchema = yup.object().shape({
+    nome: yup
+      .string()
+      .required('Este campo deve ser preenchido'),
+    imagem: yup
+      .string()
+      .required('Este campo deve ser preenchido'),
+    descricao: yup
+      .string()
+      .required('Este campo deve ser preenchido'),
+    estoque: yup
+      .number()
+      .required('Este campo deve ser preenchido'),
+    status: yup
+      .boolean()
+      .required('Este campo deve ser preenchido'),
+    preco: yup
+      .number()
+      .required('Este campo deve ser preenchido')
+  });
+  
+  module.exports = editarProdutoSchema;
