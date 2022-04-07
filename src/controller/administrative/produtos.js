1af8e2f8bd8eb564560f900f3f983e2a9f32591d
Eduardo Duarte de Sant'Anna <duartedesantanna@gmail.com>
2022-04-07 02:06:04 -0300

    Criação do controlador de produtos da area administrativa

---
+++ src/controller/administrative/produtos.js
@@ -0,0 +1,16 @@
+const knex = require('../../database/conexao');
+
+
+const cadastrarProduto = async (req, res) => {
+};
+
+const editarProduto = async (req, res) => {
+};
+
+const excluirProduto = async (req, res) => {
+};
+
+
+module.exports = {
+    cadastrarProduto, editarProduto, excluirProduto
+  }
