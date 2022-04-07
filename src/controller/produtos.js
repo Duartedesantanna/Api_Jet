eec5f790ffc331912fcb2f32ded8162a7785877c
Eduardo Duarte de Sant'Anna <duartedesantanna@gmail.com>
2022-04-07 15:34:12 -0300

    Implementação rota de listar de produtos

--- src/controller/produtos.js
+++ src/controller/produtos.js
@@ -1,6 +1,14 @@
 const knex = require('../database/conexao');
 
 const listarProdutos = async (req, res) => {
+    try {
+        const listaDeProodutos = await knex('produtos').orderBy('id');
+        
+        return res.status(200).json(listaDeProodutos);
+
+    } catch (error) {
+        return res.status(400).json({ mensagem: error.message});
+    }   
 };
 
 const abrirProduto = async (req, res) => {
