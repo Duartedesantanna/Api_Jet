3894c0f08e815182801263c0d3b3fca32a301bb5
Eduardo Duarte de Sant'Anna <duartedesantanna@gmail.com>
2022-04-13 02:58:46 -0300

    Inclusão Cors

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const knex = require('./database/conexao');
-const rotas = require('./rotas')
+const rotas = require('./rotas');
+const cors = require('cors');
+
+app.use(
+    cors({
+        origin: '*',
+        methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    })
+)
 
 app.use(express.json());
 app.use(rotas);
